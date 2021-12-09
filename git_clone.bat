93ff6f85512b6ad0c1dbfce2ac4b413e845575e5
phgoodw <75592933+phgoodw@users.noreply.github.com>
2021-12-09 05:09:18 -0500

    'Updated'

---
+++ git_clone.bat
@@ -0,0 +1,1 @@
+git clone https://github.com/pattygcoding/SMC.git
---
+++ git_push.bat
@@ -0,0 +1,5 @@
+git init
+git add -A
+git commit -m 'Updated'
+git remote add origin https://github.com/pattygcoding/SMC.git
+git push -u origin master
